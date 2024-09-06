93678e642b5bbbf38ce8aa8dddbe9e584eaa502e
Raj Kapadia <37063055+RajKKapadia@users.noreply.github.com>
2024-09-06 15:55:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ load_dotenv(find_dotenv())
 
 SCENARIO_API_KEY = os.getenv("SCENARIO_API_KEY")
 SCENARIO_SECRET_KEY = os.getenv("SCENARIO_SECRET_KEY")
+TELEGRAM_BOT_API_KEY = os.getenv("TELEGRAM_BOT_API_KEY")
+MESHY_API_KEY = os.getenv("MESHY_API_KEY")
