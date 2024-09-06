e66257a9e0e37529e0721cc64b692b161bd8f193
Raj Kapadia <37063055+RajKKapadia@users.noreply.github.com>
2024-09-06 15:54:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,87 +1,74 @@
-import time
 import requests
-import base64
-from typing import List, Any, Dict, Tuple
-import json
+import time
 
 import config
 
-
-auth_header = "Basic " + base64.b64encode(
-    f"{config.SCENARIO_API_KEY}:{config.SCENARIO_SECRET_KEY}".encode('ascii')).decode('ascii')
-
-
-base_url = 'https://api.cloud.scenario.com/v1'
-model_id = 'NKStdSjYQjaeFiTK9ps8dg'
-
-headers = {
-    'accept': 'application/json',
-    'Authorization': auth_header
-}
-
-
-def extract_image_urls(inference_data: dict[str, any]) -> Tuple[List[str], List[str]]:
-    images = inference_data.get('inference', {}).get('images', [])
-    urls = []
-    image_ids = []
-    for image in images:
-        urls.append(image["url"])
-        image_ids.append(image["id"])
-    return urls, image_ids
-
-
-def remove_background(image_id: str) -> str:
-    url = f"{base_url}/generate/remove-background"
-    payload = {"image": image_id,
-               "backgroundColor": "#ffffff"}
-    response = requests.post(url, json=payload, headers=headers)
-    response = dict(json.loads(response.text))
-    url = response.get("asset", {}).get("url", "")
-    return url
-
-
-def call_scenario_api(prompt: str) -> Dict[str, Any]:
-    response = requests.post(
-        url=f'{base_url}/generate/txt2img',
-        json={
-            'modelId': model_id,
-            'prompt': prompt + "image must have transparent background",
-            'numInferenceSteps': 30,
-            'numSamples': 2,
-            'guidance': 7.5,
-            'width': 1024,
-            'height': 1024,
-            "originalAssets": True
-        },
-        headers=headers
-    )
-    if response.status_code == 200:
-        data = response.json()
-        inference_id = data['inference']['id']
-        status = ''
-        while status not in ['succeeded', 'failed']:
-            inference_response = requests.get(
-                f'{base_url}/models/{model_id}/inferences/{inference_id}', headers=headers)
-            inference_data = dict(inference_response.json())
-            status = inference_data['inference']['status']
-            time.sleep(5)
-        if status == 'succeeded':
-            _, image_ids = extract_image_urls(inference_data=inference_data)
-            urls = []
-            for image_id in image_ids:
-                url = remove_background(image_id=image_id)
-                urls.append(url)
-            return {
-                "status": True,
-                "urls": urls
-            }
-        else:
-            return {
-                "status": False,
-                "urls": []
-            }
-    else:
-        return {
-            "status": False,
-            "urls": []
+def get_meshy_job_id(prompt: str) -> str:
+    try:
+        payload = {
+            "mode": "preview",
+            "prompt": prompt,
+            "art_style": "realistic",
+            "negative_prompt": "low quality, low resolution, low poly, ugly"
         }
+        headers = {
+            "Authorization": f"Bearer {config.MESHY_API_KEY}"
+        }
+        response = requests.post(
+            "https://api.meshy.ai/v2/text-to-3d",
+            headers=headers,
+            json=payload,
+        )
+        response.raise_for_status()
+        response = response.json()
+        return response["result"]
+    except:
+        return ""
+
+
+def retriev_meshy_job_by_id(task_id: str) -> tuple[list[str], list[str], list[str], bool]:
+    headers = {
+        "Authorization": f"Bearer {config.MESHY_API_KEY}"
+    }
+    flag = True
+    status = False
+    count = 0
+    url = []
+    video_url = []
+    model_urls = []
+    while flag:
+        response = requests.get(
+            f"https://api.meshy.ai/v2/text-to-3d/{task_id}",
+            headers=headers,
+        )
+        response.raise_for_status()
+        if response.status_code == 200:
+            response = response.json()
+            print(response)
+            if response["status"] == "SUCCEEDED":
+                url.append(response["thumbnail_url"])
+                video_url.append(response["video_url"])
+                model_urls.append(response["model_urls"])
+                flag = False
+                status = True
+            elif count >= 30:
+                flag = False
+                status = False
+            else:
+                count += 1
+        time.sleep(3)
+    return url, video_url, model_urls, status
+
+
+def call_meshy_api(prompt: str) -> tuple[list[str], list[str], list[str], bool]:
+    task_id = get_meshy_job_id(prompt=prompt)
+    print(task_id)
+    url = []
+    video_url = []
+    model_urls = []
+    if task_id == "":
+        return url, video_url, model_urls, False
+    else:
+        url, video_url, model_urls, status = retriev_meshy_job_by_id(
+            task_id=task_id)
+        return url, video_url, model_urls, status
